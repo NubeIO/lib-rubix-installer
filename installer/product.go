609beddfac2c79b99dca086ea1f03ce786cbd514
NubeDev <aidan@enviroservices.com.au>
2022-08-11 15:56:44 +1000

    update to product

--- installer/product.go
+++ installer/product.go
@@ -11,7 +11,7 @@ type Product struct {
 	EdgeVersion  string `json:"edge_version"`
 	FlowVersion  string `json:"flow_version"`
 	ImageVersion string `json:"image_version"`
-	Product      string `json:"product"` // RubixCompute, RubixComputeIO, RubixCompute5, Server, Edge28, Nuc
+	Product      string `json:"product"` // RubixCompute, RubixComputeIO, RubixCompute5, Server, Edge28, Nuc  see https://github.com/NubeIO/lib-command/blob/master/product/product.go#L7
 	Arch         string `json:"arch"`    // armv7 amd64
 	OS           OS     `json:"os"`      // Linux, Windows, Darwin
 }
