4ec791f0ffd0d576da108f8d4c0c552c40752679
NubeDev <aidan@enviroservices.com.au>
2022-08-29 11:43:41 +1000

    delete old app on install

--- installer/upload_test.go
+++ installer/upload_test.go
@@ -2,25 +2,23 @@ package installer
 
 import (
 	"fmt"
-	fileutils "github.com/NubeIO/lib-dirs/dirs"
 	"testing"
 )
 
 func TestApp_uploadApp(t *testing.T) {
 	//var err error
-	homeDir, _ := fileutils.Dir()
-	fmt.Println(homeDir)
-	app := New(&App{DataDir: "/data", FilePerm: nonRoot, HostDownloadPath: fmt.Sprintf("%s/Downloads", homeDir)})
-
-	appName := "rubix-wires"
-	appInstallName := "wires-builds"
-	serviceName := "nubeio-rubix-wires"
-	appVersion := "v2.7.2"
-	appZip := "wires-builds-2.7.2.zip"
-
-	fmt.Println(app, appName, appInstallName, serviceName, appVersion, appZip)
-
+	///data/tmp/tmp_D2F8D40F77F8/nubeio-rubix-app-lora-serial-py-0.0.1-fefc55ac.armv7.zip dest:/data/rubix-service/apps/install/lora-driver/v0.0.1
 	//err = app.uploadApp(appName, appInstallName, appVersion)
 	//
 	//fmt.Println(err)
+
+	app := New(&App{DataDir: "/data", FilePerm: nonRoot})
+
+	zip, err := app.unZip("/data/tmp/tmp_D2F8D40F77F8/nubeio-rubix-app-lora-serial-py-0.0.1-fefc55ac.armv7.zip", "/data/rubix-service/apps/install/lora-driver/v0.0.1")
+	fmt.Println(err)
+	fmt.Println(zip)
+	if err != nil {
+		return
+	}
+
 }
