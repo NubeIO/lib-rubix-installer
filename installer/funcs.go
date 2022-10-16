dcfcee54678725d30a655697235396852d5dfb80
RaiBnod <binodrayee@gmail.com>
2022-10-16 10:54:12 +0545

    Fix: move distribution files from function MoveOneLevelInsideFileToOutside

--- installer/funcs.go
+++ installer/funcs.go
@@ -94,26 +94,56 @@ func extractZipFile(f *zip.File, destination string) error {
 	return nil
 }
 
-func MoveOneLevelInsideFileToOutside(file string) error {
+// MoveOneLevelInsideFileToOutside needs to move all these files:
+// - /data/rubix-service/apps/install/wires-builds/v2.7.4/NubeIO-wires-builds-5082d47/README.md
+// - /data/rubix-service/apps/install/wires-builds/v2.7.4/NubeIO-wires-builds-5082d47/rubix-wires/*
+// To: /data/rubix-service/apps/install/wires-builds/v2.7.4
+func MoveOneLevelInsideFileToOutside(file string) error { // /data/rubix-service/apps/install/wires-builds/v2.7.4
 	fileInfo, err := os.Stat(file)
 	if err != nil {
 		return err
 	}
-	if fileInfo.IsDir() {
+	if fileInfo.IsDir() { // /data/rubix-service/apps/install/wires-builds/v2.7.4
 		files, err := ioutil.ReadDir(file)
 		if err != nil {
 			return err
 		}
 		for _, f := range files {
-			if f.IsDir() {
-				source := path.Join(file, f.Name())
-				destination := file
-				// risk to copy if we have same folder inside as its current folder name
-				err = fileutils.Copy(source, destination)
+			if f.IsDir() { // /data/rubix-service/apps/install/wires-builds/v2.7.4/NubeIO-wires-builds-5082d47
+				file2 := path.Join(file, f.Name()) // /data/rubix-service/apps/install/wires-builds/v2.7.4/NubeIO-wires-builds-5082d47
+				fileInfo2, err := os.Stat(file2)
 				if err != nil {
 					return err
 				}
-				err = os.RemoveAll(source)
+				if fileInfo2.IsDir() { // /data/rubix-service/apps/install/wires-builds/v2.7.4/NubeIO-wires-builds-5082d47
+					files2, err := ioutil.ReadDir(file2)
+					if err != nil {
+						return err
+					}
+					for _, f2 := range files2 { // /data/rubix-service/apps/install/wires-builds/v2.7.4/NubeIO-wires-builds-5082d47/*
+						file3 := path.Join(file2, f2.Name())
+						fileInfo3, err := os.Stat(file3)
+						if err != nil {
+							return err
+						}
+						if fileInfo3.IsDir() {
+							source := file3     // /data/rubix-service/apps/install/wires-builds/v2.7.4/NubeIO-wires-builds-5082d47/rubix-wires
+							destination := file // /data/rubix-service/apps/install/wires-builds/v2.7.4
+							err = fileutils.Copy(source, destination)
+							if err != nil {
+								return err
+							}
+						} else {
+							destination := path.Join(file, fileInfo3.Name())
+							// /data/rubix-service/apps/install/wires-builds/v2.7.4/NubeIO-wires-builds-5082d47/README.md => /data/rubix-service/apps/install/wires-builds/v2.7.4/README.md
+							err = fileutils.Copy(file3, destination)
+							if err != nil {
+								return err
+							}
+						}
+					}
+				}
+				err = os.RemoveAll(file2)
 				if err != nil {
 					return err
 				}
